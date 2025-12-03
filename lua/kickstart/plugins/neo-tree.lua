40eb787aee93237dc0279355cae5c9c80ee795f5
Matthew Recinto <recinto.matt@gmail.com>
2025-12-03 10:10:35 -0800

    configured neo-tree to auto close when opening files

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
@@ -19,6 +19,15 @@ return {
         mappings = {
           ['\\'] = 'close_window',
         },
+      },
+    },
+
+    event_handlers = {
+      {
+        event = 'file_open_requested',
+        handler = function()
+          require('neo-tree.command').execute { action = 'close' }
+        end,
       },
     },
   },
